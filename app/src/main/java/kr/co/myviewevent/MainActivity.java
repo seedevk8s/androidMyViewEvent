25c99e905f13aac5976c3e96686fdfd034feb4c7
seedevk8s <seedevk8s@gmail.com>
2022-02-06 22:33:58 +0900

    터치 이벤트 재정의 앱

---
+++ app/src/main/java/kr/co/myviewevent/MainActivity.java
@@ -0,0 +1,75 @@
+package kr.co.myviewevent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher);
+
+        MyView myView = new MyView(this);
+        setContentView(myView);
+    }
+
+    private class MyView extends View {
+
+        int key;
+        String str;
+        int x, y;
+
+        public MyView(Context context) {
+            super(context);
+            setBackgroundColor(Color.BLUE);
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            x = (int) event.getX(0);
+            y = (int) event.getY(0);
+            invalidate();
+            return super.onTouchEvent(event);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+
+            Paint paint = new Paint();
+            paint.setTextSize(50);
+            canvas.drawCircle(x, y, 30, paint);
+            canvas.drawText("(" +x+ ", " +y+ ") 에서 터치 이벤트가 발생하였음.", x, y + 100, paint);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
